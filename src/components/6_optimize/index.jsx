e0cabc8af4f34a430114fa77b889e245332660ac
Nora.Hu <nora.h@thinkwithu.com>
2023-01-05 16:36:14 +0800

    使用PureComponent重写shouldComponentUpdate

--- src/components/6_optimize/index.jsx
+++ src/components/6_optimize/index.jsx
@@ -1,16 +1,27 @@
 import React, { Component,PureComponent } from "react";
 import "./index.css";
 
-export default class Parent extends Component {
-  state = { carName: "奔驰c" };
+export default class Parent extends PureComponent {
+  state = { carName: "奔驰c",persons:['小李','小王','小红'] };
 
   changeCar = () => {
-    this.setState({ carName: "迈巴赫" });
+    // this.setState({ carName: "迈巴赫" });
+
     // this.setState({});
+
+    const obj = this.state;
+    obj.carName = '迈巴赫'
+    console.log(obj === this.state)   //true
+    this.setState(obj)
   };
 
+  addPerson = ()=>{
+    const {persons} = this.state
+    this.setState({persons:['小白',...persons]})
+  }
+
   // 手动控制阀门
-  shouldComponentUpdate(nextProps,nextState) {
+/*   shouldComponentUpdate(nextProps,nextState) {
     console.log('this.props',this.props)
     console.log('this.state',this.state) //目前的props和state
 
@@ -19,7 +30,7 @@ export default class Parent extends Component {
     // if(this.state.carName === nextState.carName) return false
     // else return true
     return !this.state.carName === nextState.carName
-  }
+  } */
 
   render() {
     const { carName } = this.state;
@@ -27,23 +38,25 @@ export default class Parent extends Component {
     return (
       <div className="parent">
         <h1>我是Parent组件</h1>
+        <h2>{this.state.persons}</h2>
         <h2>我的车是：{carName}</h2>
         <button onClick={this.changeCar}>点我换车</button>
-        <Child carName="奥拓" />
+        <button onClick={this.addPerson}>添加一人</button>
+        <Child carName={carName} />
       </div>
     );
   }
 }
 
-class Child extends Component {
-  shouldComponentUpdate(nextProps,nextState) {
+class Child extends PureComponent {
+/*   shouldComponentUpdate(nextProps,nextState) {
     console.log('子组件this.props',this.props)
     console.log('子组件nextProps',nextProps)//接下来变化的目标props，目标state
 
     // if(this.props.carName === nextProps.carName) return false
     // else return true
     return !this.props.carName === nextProps.carName
-  }
+  } */
   render() {
     console.log("子组件render");
     return (
